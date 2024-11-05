c60d7d75bba52bfb468f2839ee4c911e561f8c64
SSANTA11 <ssanta011205@skuniv.ac.kr>
2024-11-06 00:17:21 +0900

    11월 06일 백준 온라인 학습

--- practice176.py
+++ practice176.py
@@ -1,10 +1,17 @@
 inp=input().split()
 a=int(inp[0])
 b=int(inp[1])
-
-if b%a==0:
-    print("factor")
-elif a%b==0:
-    print("multiple")
-else:
-    print("neither")
+while (a!=0 and b!=0):
+    if a==0 and b==0:
+        break
+    elif a==0 or b==0:
+        print("neither")
+    elif b%a==0:
+        print("factor")
+    elif a%b==0:
+        print("multiple")   
+    else:
+        print("neither")
+    inp=input().split()
+    a=int(inp[0])
+    b=int(inp[1])    
