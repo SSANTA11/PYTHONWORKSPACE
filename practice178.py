d5fdf21425ab825f7f5f431dc9440be88723c74f
SSANTA11 <ssanta011205@skuniv.ac.kr>
2024-12-05 11:57:04 +0900

    12월 05일 PCCE 문제풀이

---
+++ practice178.py
@@ -0,0 +1,10 @@
+def solution(cpr):
+    answer = []
+    basic_order = ["check", "call", "pressure", "respiration", "repeat"]
+    for action in cpr:
+        for i in range(5):
+            if action == basic_order[i]:
+                answer.append(i+1)
+    return answer
+
+print(solution(["call", "respiration", "repeat", "check", "pressure"]))
