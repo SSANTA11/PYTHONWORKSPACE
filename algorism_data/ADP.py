02e2d2dddcfe63acfb479281667a0f7d9c4bc3ac
SSANTA11 <ssanta011205@skuniv.ac.kr>
2025-04-02 20:39:13 +0900

    sss

--- algorism_data/ADP.py
+++ algorism_data/ADP.py
@@ -11,15 +11,40 @@
 # 사용자 정의 함수
 
 # 함수의 정의와 호출
-def find_min_max(A):
-    min=A[0]
-    max=A[0]
-    for i in range(1,len(A)):
-        if max<A[i]:
-            max=A[i]
-        if min>A[i]:
-            min=A[i]
-    return min, max
-data=[1,2,3,4,5,76,7000]
-x,y=find_min_max(data)
-print("(min, max)= ", (x, y))
+    # def find_min_max(A):
+    #     min=A[0]
+    #     max=A[0]
+    #     for i in range(1,len(A)):
+    #         if max<A[i]:
+    #             max=A[i]
+    #         if min>A[i]:
+    #             min=A[i]
+    #     return min, max
+    # data=[1,2,3,4,5,76,7000]
+    # x,y=find_min_max(data)
+    # print("(min, max)= ", (x, y))
+
+
+# 두 결과 반환 가능한 파이썬
+    # nums=input()
+    # map(int,nums.split())
+    # for i in range(len(nums)):
+    #     print(f'{i}', end=' ')
+    # print('\n')
+    # for i in range(len(nums)):
+    #     print(f'{type(i)}', end=' ')
+
+# 지정된 범위의 수를 합한 결과를 반환하는 함수
+    # def sum_range(begin, end, step=1):
+    #     sum=0
+    #     for n in range(begin, end, step):
+    #         sum+=n
+    #     return sum
+
+def sum_recur(i):
+    if i<1:
+        return 0
+    else:
+        return i+sum_recur(i-1)
+
+print(sum_recur(10))
--- algorism_data/algorism_data_structure_week2_1.py
+++ algorism_data/algorism_data_structure_week2_1.py
@@ -10,6 +10,7 @@ def fac(num):
 n=int(input())
 print(fac(n))
 
+# 두 결과 반환 가능한 파이썬
 nums=input()
 map(int,nums.split())
 for i in range(len(nums)):
@@ -17,3 +18,20 @@ for i in range(len(nums)):
 print('\n')
 for i in range(len(nums)):
     print(f'{type(i)}', end=' ')
+
+# 지정된 범위의 수를 합한 결과를 반환하는 함수
+def sum_range(begin, end, step=1):
+    sum=0
+    for n in range(begin, end, step):
+        sum+=n
+    return sum
+
+def sum_recur(n):
+    sum=0
+    if n<1:
+        return sum;
+    else:
+        sum=n+sum_recur(n-1)
+    return sum
+
+print(sum_recur(10))
