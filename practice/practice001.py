da5c00fd667460d4f2803c6a7463ce50f3ffb7c6
SSANTA11 <ssanta011205@skuniv.ac.kr>
2026-01-30 03:01:04 +0900

    :white_check_mark: 01/30

--- practice/practice001.py
+++ practice/practice001.py
@@ -1,1 +1,17 @@
-print("hello world...!")
+while(True):
+    n=input().split()
+    if (n[0]==n[1]==n[2] and n[0]=='0'):
+        break;
+    l=[]
+    for i in range(3):
+       l.append(int(n[i]))
+    l.sort()
+    m=l.pop()
+    sum=0
+    for e in l:
+        sum+=e**2
+    if (m**2==sum):
+        print("right")
+    else:
+        print("wrong")
+
