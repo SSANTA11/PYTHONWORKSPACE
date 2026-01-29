c5177d897411fff9efe1dfd163262f148ac24e66
SSANTA11 <ssanta011205@skuniv.ac.kr>
2026-01-30 03:01:05 +0900

    :white_check_mark: 01/30

--- practice/beakj/BJ1037.py
+++ practice/beakj/BJ1037.py
@@ -4,6 +4,6 @@ words,max_count=map(int,input().split())
 
 for _ in range(words):
     word=input()
-    if len(word)>=max_count:
-        words_count[word]+=1
+    if words_count. len(word)>=max_count:
+        words_count[word]=0
 
